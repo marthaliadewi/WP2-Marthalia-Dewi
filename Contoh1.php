aad0a0353c12f0413787dd588d3df0523f8e2818
marthaliadewi <90971456+marthaliadewi@users.noreply.github.com>
2021-09-19 11:51:55 +0700

    Add files via upload

---
+++ Contoh1.php
@@ -0,0 +1,12 @@
+<?php
+class Contoh1 extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>Perkenalkan</h>";
+        echo"Nama saya Marthalia Dewi
+            Saya tinggal di daerah Bogor
+            Olah raga yang saya sukai adalah
+            bulutangkis";
+    }
+}
